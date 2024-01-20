cd3f5f7bac6d334a6c38f6171213c459c29dcf40
nyannn81x03 <ntnhan0812a@gmail.com>
2024-01-20 13:37:45 +0700

    ip

--- lib/ip/ip.dart
+++ lib/ip/ip.dart
@@ -1,3 +1,12 @@
 
 // var u = 'http://172.16.7.178:8081' ;
-var u = 'http://192.168.1.52:8081' ;
+import 'package:shared_preferences/shared_preferences.dart';
+
+var u = 'http://192.168.1.81:8081' ;
+
+getIdSup() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  //Return int
+  int? intValue = prefs.getInt('idSup');
+  return intValue;
+}
